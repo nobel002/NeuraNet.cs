d35c77389274b7d06a1c7ca0864162ed86403e6f
nobel002 <emileerauw@gmail.com>
2021-12-21 13:38:43 +0100

    created a whole bunch of new functions

--- NeuraNet.cs
+++ NeuraNet.cs
@@ -11,11 +11,12 @@ namespace NeuraNet
   class NetCreator
   {
     public int[] shape { get; set; }
-    public float[][] nodes { get; set; }
-    public float[][][] synapses { get; set; }
+    public double[][] nodes { get; set; }
+    public double[][][] synapses { get; set; }
     public Net[] enviroment { get; set; }
     public int numberOfSpecimen { get; set; }
     public int trainingTime { get; set; }
+    private Random rnd = new Random();
 
     public NetCreator(int[] shape, int numberOfSpecimen, int trainingTime)
     {
@@ -31,44 +32,88 @@ namespace NeuraNet
     public void Init()
     {
       Console.WriteLine("Initing...");
-      // Console.WriteLine(this.shape);
       CreateNodes();
+      //FillNodes();
       Console.WriteLine("Created Nodes...");
       CreateSynapses();
-      Console.WriteLine("Created synapses...");
+      FillSynapses();
+      Console.WriteLine("Created Synapses...");
     }
 
     /// <summary>
     /// This creates the nodes in this object.
+    /// And fills them with all 0's.
     /// </summary>
     void CreateNodes()
     {
-      this.nodes = new float[this.shape.Length][];
+      this.nodes = new double[this.shape.Length][];
       for (int i = 0; i <= shape.Length - 1; i++)
       {
-        this.nodes[i] = new float[this.shape[i]];
+        this.nodes[i] = new double[this.shape[i]];
         //Array.Clear(this.nodes[i], 0, this.shape[i]);
       }
     }
 
+    /// <summary>
+    /// Creates the synapses and fills them with 0's;
+    /// </summary>
     void CreateSynapses()
     {
-      this.synapses = new float[this.shape.Length - 1][][];
+      this.synapses = new double[this.shape.Length - 1][][];
       for (int i = 0; i <= this.shape.Length - 2; i++)
       {
-        this.synapses[i] = new float[this.shape[i + 1]][];
+        this.synapses[i] = new double[this.shape[i + 1]][];
         for (int j = 0; j <= this.synapses[i].Length - 1; j++)
         {
-          this.synapses[i][j] = new float[this.shape[i]];
+          this.synapses[i][j] = new double[this.shape[i]];
           //Array.Clear(this.synapses[i][j], 0, this.shape[i]);
         }
       }
+    }
+
+    /// <summary>
+    /// Fills the synapses with noise.
+    /// </summary>
+    void FillSynapses()
+    {
+      for (int i = 0; i <= this.synapses.Length - 1; i++)
+      {
+        for (int j = 0; j <= this.synapses[i].Length - 1; j++)
+        {
+          for (int k = 0; k <= this.synapses[i][j].Length - 1; k++)
+          {
+            this.synapses[i][j][k] = rnd.NextDouble();
+          }
+        }
+      }
+    }
+
+    public Net[] CreateEnv()
+    {
+      for (int i = 0; i <= numberOfSpecimen - 1; i++)
+      {
+        this.enviroment[i] = new Net(this.nodes, this.synapses);
+        // To ensure that we generate different Nets each time.
+        this.FillSynapses();
+      }
+      return this.enviroment;
     }
   }
 
   class Net
   {
     public int score { get; set; }
-    public float[][][] synapses { get; set; }
+    public double[][] nodes { get; set; }
+    public double[][][] synapses { get; set; }
+
+    public Net(double[][] nodes, double[][][] synapses)
+    {
+      this.nodes = nodes;
+      this.synapses = synapses;
+    }
+
+    void Use(){
+      //TODO(nobel) write the use function so we can calculate everything.
+    }
   }
 }
--- Program.cs
+++ Program.cs
@@ -12,8 +12,6 @@ namespace ai
       const int trainTime = (int)10e3;
       NetCreator test = new NetCreator(shape, specimenCount, trainTime);
       test.Init();
-      float[][] nodes = test.nodes;
-      Console.WriteLine(nodes);
     }
   }
 }
